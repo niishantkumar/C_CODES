6d7610b701da21ad3a5612be4e6554c2c7028b1e
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-04 17:40:26 +0530

    prac2.c modified

--- searching/prac2.c
+++ searching/prac2.c
@@ -1,1 +1,49 @@
 // binary search
+#include <stdio.h>
+
+// func for binary search
+int binarySearch(int[], int, int);
+
+void main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+
+    int index = binarySearch(arr, 6, 6);
+
+    if (index < 0)
+    {
+        printf("No match !");
+    }
+    else
+    {
+        printf("Found at index %d", index);
+    }
+}
+
+int binarySearch(int arr[], int key, int size)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid;
+
+    while (start <= end)
+    {
+        mid = (start + end) / 2;
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+
+        if (key < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+
+    return -1;
+}
