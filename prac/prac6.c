c189b15309e4c357e5e94b37ca332afccbe076c7
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-05 20:20:06 +0530

    prac6.c modified

--- prac/prac6.c
+++ prac/prac6.c
@@ -1,1 +1,44 @@
 //transpose of a matrix
+
+#include <stdio.h>
+
+void main () {
+    int r,c;
+    printf ("Enter the number of rows : ");
+    scanf ("%d",&r);
+    printf ("Enter the number of columns : ");
+    scanf ("%d",&c);
+    int arr[r][c];
+    int arr1 [c][r];
+    for (int i = 0; i<r;i++) {
+        for (int j = 0; j<c;j++) {
+        printf ("Enter element at Position %d,%d : ",i+1,j+1);
+        scanf ("%d",&arr[i][j]);
+        }
+    }
+    printf ("\n");
+
+    printf ("mtrx is:\n");
+    for (int i = 0; i<r;i++) {
+        for (int j = 0; j<c;j++) {
+        printf ("%d ",arr[i][j]);
+        }
+        printf ("\n");
+    }
+    
+
+    for (int i = 0; i<c;i++) {
+        for (int j = 0; j<r;j++) {
+        arr1[i][j] = arr [j][i];
+        }
+    }
+
+    printf ("\n");
+    printf ("The transpose mtrx is :\n");
+    for (int i = 0; i<c;i++) {
+        for (int j = 0; j<r;j++) {
+        printf ("%d ",arr1[i][j]);
+        }
+        printf ("\n");
+    }
+}
