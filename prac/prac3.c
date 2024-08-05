0eda77a43acfb3f146779f861b8318da8c4abf47
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-05 19:28:35 +0530

    prac3.c modified

--- prac/prac3.c
+++ prac/prac3.c
@@ -1,7 +1,43 @@
-//Find the GCD of two numbers.
+// Find the GCD of two numbers.
 
 #include <stdio.h>
 
-int main(){
-    
+// func to cal gcd
+int gcd(int num1, int num2);
+
+int main()
+{
+    int num1, num2;
+    printf("Enter 1st number : ");
+    scanf("%d", &num1);
+    printf("Enter 2nd number : ");
+    scanf("%d", &num2);
+
+    int gcd1 = gcd(num1, num2);
+
+    if (gcd1 == 0)
+    {
+        printf("No common factor");
+    }
+    else
+    {
+        printf("Gcd of %d & %d is : %d", num1, num2, gcd1);
+    }
+}
+
+int gcd(int num1, int num2)
+{
+    int min = num1 < num2 ? num1 : num2;
+
+    int gcd1 = 0;
+
+    for (int i = 1; i <= min; i++)
+    {
+        if (num1 % i == 0 && num2 % i == 0)
+        {
+            gcd1 = i;
+        }
+    }
+
+    return gcd1;
 }
