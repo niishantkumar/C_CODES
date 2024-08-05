fa5a568631b1158d5c57f50a4cc48710da23e37e
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-05 19:31:20 +0530

    prac3.c modified

--- prac/prac3.c
+++ prac/prac3.c
@@ -29,15 +29,15 @@ int gcd(int num1, int num2)
 {
     int min = num1 < num2 ? num1 : num2;
 
-    int gcd1 = 0;
+    int gcd = 0;
 
     for (int i = 1; i <= min; i++)
     {
         if (num1 % i == 0 && num2 % i == 0)
         {
-            gcd1 = i;
+            gcd = i;
         }
     }
 
-    return gcd1;
+    return gcd;
 }
