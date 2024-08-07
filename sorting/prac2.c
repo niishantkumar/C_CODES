a44e206ccefb68ce3db8fa646983d8a9dec0c49b
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-07 22:05:42 +0530

    prac2.c modified

--- sorting/prac2.c
+++ sorting/prac2.c
@@ -1,1 +1,48 @@
-//insertion sort
+// insertion sort
+#include <stdio.h>
+
+// func prototype
+void insertionSort(int[], int);
+
+void main()
+{
+    int size;
+    printf("Enter size of array : ");
+    scanf("%d", &size);
+
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter element at %d : ", i);
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
+
+    insertionSort(arr,size);
+}
+
+void insertionSort(int arr[], int size)
+{
+    int temp;
+    int curr, prev;
+
+    for (int i = 1; i < size; i++)
+    {
+        curr = arr[i];
+        prev = i - 1;
+
+        while (curr < arr[prev] && prev >= 0)
+        {
+            arr[prev + 1] = arr[prev];
+
+            prev--;
+        }
+
+        arr[prev + 1] = curr;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
