b5394777cc9f18e040d09112355ba6bc9e59a552
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-07 20:08:46 +0530

    prac1.c modified

--- sorting/prac1.c
+++ sorting/prac1.c
@@ -1,1 +1,46 @@
-//bubble sort
+// bubble sort
+#include <stdio.h>
+
+// func prototype
+void bubbleSort(int[], int);
+
+void main()
+{
+    int size;
+    printf("Enter size of array : ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter element at %d : ", i);
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
+
+    bubbleSort(arr, size);
+}
+
+void bubbleSort(int arr[], int size)
+{
+    int temp;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
