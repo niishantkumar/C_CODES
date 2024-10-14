561a6409968c343e589988de9e583d24a3fe57cb
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-10-14 18:31:35 +0530

    prac4.c modified

--- sorting/prac4.c
+++ sorting/prac4.c
@@ -1,1 +1,83 @@
-//merge sort
+// merge sort
+
+#include <stdio.h>
+
+void merge(int arr[], int, int, int);
+void mergeSort(int arr[], int, int);
+
+void main()
+{
+    int arr[10], i, n, j, k;
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of array :\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("arr[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("\nThe sorted array is :\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+
+void merge(int arr[], int beg, int mid, int end)
+{
+    int i = beg, j = mid + 1, index = beg, temp[4], k;
+
+    while ((i <= mid) && (j <= end))
+    {
+        if (arr[i] < arr[j])
+        {
+            temp[index] = arr[i];
+            i++;
+        }
+        else
+        {
+            temp[index] = arr[j];
+            j++;
+        }
+        index++;
+    }
+
+    if (i > mid)
+    {
+        while (j <= end)
+        {
+            temp[index] = arr[j];
+            j++;
+            index++;
+        }
+    }
+    else
+    {
+        while (i <= mid)
+        {
+            temp[index] = arr[i];
+            i++;
+            index++;
+        }
+    }
+
+    for (k = beg; k < index; k++)
+        arr[k] = temp[k];
+}
+
+void mergeSort(int arr[], int beg, int end)
+{
+    int mid;
+    if (beg < end)
+    {
+        mid = (beg + end) / 2;
+        mergeSort(arr, beg, mid);
+        mergeSort(arr, mid + 1, end);
+        merge(arr, beg, mid, end);
+    }
+}
