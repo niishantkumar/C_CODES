9bdc146b0260040aac5277fdb6ad9ce22f87092e
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-10-28 23:38:20 +0530

    prac2.c modified

--- array/prac2.c
+++ array/prac2.c
@@ -1,1 +1,29 @@
-//to check sum of which 2 elements is 15
+// to check sum of which 2 elements is 15
+
+#include <stdio.h>
+
+void main()
+{
+    int arr[] = {1, 2, 3, 5, 7, 10, 11, 15};
+
+    int left = 0, right = sizeof(arr) / sizeof(arr[0]) - 1;
+
+    while (left < right)
+    {
+        if ((arr[left] + arr[right]) == 15)
+        {
+            printf("Element at index %d & %d", left, right);
+            return;
+        }
+        else if (arr[left] + arr[right] < 15)
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+
+    printf("No such two elements");
+}
