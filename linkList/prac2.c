979d8a7c83bc6876fa4b6f0ea663cc12013383de
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-11-18 22:23:37 +0530

    prac2.c modified

--- linkList/prac2.c
+++ linkList/prac2.c
@@ -1,8 +1,52 @@
+// dll creation
 #include <stdio.h>
 #include <stdlib.h>
 
 struct node
 {
     int data;
-    struct node *link;
+    struct node *next;
+    struct node *prev;
 };
+
+typedef struct node *Node;
+
+void main()
+{
+    Node ptr, head, trv;
+
+    ptr = (Node)malloc(sizeof(struct node));
+
+    head = ptr;
+
+    printf("ENter data : ");
+    scanf("%d", &(ptr->data));
+    ptr->next = NULL;
+
+    getchar();
+
+    char ch;
+    printf("Do you want to enter more data ? ");
+    scanf("%c", &ch);
+
+    while (ch == 'y' || ch == 'Y')
+    {
+        trv = ptr;
+        ptr->next = (Node)malloc(sizeof(struct node));
+        ptr = ptr->next;
+        printf("ENter data : ");
+        scanf("%d", &(ptr->data));
+        ptr->prev = trv;
+        getchar();
+        printf("Do you want to enter more data ? ");
+        scanf("%c", &ch);
+    }
+
+    ptr->prev = NULL;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+}
