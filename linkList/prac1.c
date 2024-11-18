d49859f0a9c8e70552bd960298b4128e638ffecd
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-11-18 22:18:13 +0530

    prac1.c modified

--- linkList/prac1.c
+++ linkList/prac1.c
@@ -1,1 +1,48 @@
-//single linklist
+// sll creation
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node *Node;
+
+void main()
+{
+    Node ptr, head;
+
+    ptr = (Node)malloc(sizeof(struct node));
+
+    head = ptr;
+
+    printf("ENter data : ");
+    scanf("%d", &(ptr->data));
+
+    getchar();
+
+    char ch;
+    printf("Do you want to enter more data ? ");
+    scanf("%c", &ch);
+
+    while (ch == 'y' || ch == 'Y')
+    {
+        ptr->next = (Node)malloc(sizeof(struct node));
+        ptr = ptr->next;
+        printf("ENter data : ");
+        scanf("%d", &(ptr->data));
+        getchar();
+        printf("Do you want to enter more data ? ");
+        scanf("%c", &ch);
+    }
+
+    ptr->next = NULL;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+}
